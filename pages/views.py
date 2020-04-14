ad8571c1324224a6f68d211a059fbff8c72547ee
toseef847 <toseefhasan@gmail.com>
2020-04-15 01:12:22 +0500

    Home page , About page completed, pagination added

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 def index(requeset):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
     # return HttpResponse('<h1>Hello World!</h1>')
-    return render(requeset, 'pages/index.html')
+    return render(requeset, 'pages/index.html', context)
 
 def about(requeset):
+    # All realtors
+    all_realtors = Realtor.objects.order_by('hire_date')
+
+    # Realtor(s) of the month
+    mvp_realtors = Realtor.objects.filter(is_mvp=True)
+
+    # The context object
+    context = {
+        'realtors': all_realtors,
+        'mvp_realtor': mvp_realtors,
+    }
     # return HttpResponse("You're at the pages")
-    return render(requeset, 'pages/about.html')
+    return render(requeset, 'pages/about.html', context)

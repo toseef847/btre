c6bf103cb64e92118358800ebf08ce8aa4cf72d0
toseef847 <toseefhasan@gmail.com>
2020-04-18 16:12:54 +0500

    fixed search results, added input preserving

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
 from . models import Listing
+from .choices import *
 
 # Create your views here.
 
@@ -20,11 +21,52 @@ def index(request):
 
 def listing(request, listing_id):
 
+    listing = get_object_or_404(Listing, pk=listing_id)
+
     context = {
-        'listing_id': listing_id
+        'listing': listing
     }
     return render(request, 'listings/listing.html', context)
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
 
-    return render(request, 'listings/search.html')
+    # Search for keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET.get('keywords')
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains = keywords)
+    
+    # Search for City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact = city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact = state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte = bedrooms)
+    
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte= price)
+
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'listings': queryset_list,
+        'values': request.GET,
+    }
+
+    return render(request, 'listings/search.html', context)

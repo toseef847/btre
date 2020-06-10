31d020e7514a021fca96c23b8db3290221fe3ded
= <=>
2020-06-10 12:57:05 +0500

    Changed urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login', views.login, name = 'login'),
     path('logout', views.logout, name = 'logout'),
     path('dashboard', views.dashboard, name = 'dashboard'),
+    path('dashboard1', views.dashboard1, name = 'dashboard1'),
 ]
